d10b5f5ddcb05d87ca1f00f3704ddf283c6f72e2
Thanh Phong Pham <tppham95@gmail.com>
2022-03-27 13:34:56 +0200

    added an error page for parential guidance

---
+++ src/pages/ErrorPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-col mx-4 justify-center min-h-full items-stretch">
+    <h1 class="text-center text-4xl font-semibold">
+      You seem lost, should we call your parents?
+    </h1>
+    <div class="flex flex-col items-center m-2">
+      <img src="../assets/lost-child.jpg" height="428" width="612" />
+    </div>
+    <button
+      @click="$router.push('/')"
+      class="flex-grow-0 bg-blue-600 py-2 text-white font-semibold"
+    >
+      asd
+    </button>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style>
+body,
+html,
+div#app {
+  height: 100%;
+}
+
+div#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import ProductOverview from "@/pages/ProductOverview.vue";
 import ProductDetails from "@/pages/ProductDetails.vue";
+import ErrorPage from "@/pages/ErrorPage.vue";
 
 const routes = [
   { path: "/", component: ProductOverview, name: "ProductOverview" },
   { path: "/details/:id", component: ProductDetails, name: "ProductDetails" },
-  { path: "/:catchAll(.*)", redirect: "/"}
+  { path: "/:catchAll(.*)", component: ErrorPage, name: "ErrorPage"}
 ];
 
 const router = createRouter({
