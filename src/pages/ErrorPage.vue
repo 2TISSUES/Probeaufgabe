c4cb627a19aee56b1da11fc91a1390e14d942a70
Thanh Phong Pham <tppham95@gmail.com>
2022-03-27 16:08:16 +0200

    design breaking bug fix plus some visual extras

--- src/pages/ErrorPage.vue
+++ src/pages/ErrorPage.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="flex flex-col mx-4 justify-center min-h-full items-stretch">
     <h1 class="text-center text-4xl font-semibold">
-      You seem lost, should we call your parents?
+      You seem lost, should we bring you home?
     </h1>
     <div class="flex flex-col items-center m-2">
       <img src="../assets/lost-child.jpg" height="428" width="612" />
     </div>
     <button
       @click="$router.push('/')"
-      class="flex-grow-0 bg-blue-600 py-2 text-white font-semibold"
+      class="
+        flex-grow-0
+        bg-blue-600
+        py-2
+        text-white
+        font-semibold
+        hover:bg-blue-700
+        rounded-md
+      "
     >
-      asd
+      HOME 🏠
     </button>
   </div>
 </template>
@@ -21,11 +29,5 @@ body,
 html,
 div#app {
   height: 100%;
-}
-
-div#app {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
 }
 </style>
